910f472ecb409a695440c449b75c336ab3c1bcae
BiGPiG <Bogdan.ars.15@yandex.ru>
2025-09-30 22:29:48 +0300

    feat: update function create_fragment

--- kompas_service.py
+++ kompas_service.py
@@ -2,6 +2,7 @@ import os
 
 import win32com.client
 import pythoncom
+from win32com.client import gencache
 
 
 class KompasService:
@@ -9,11 +10,13 @@ class KompasService:
         try:
             pythoncom.CoInitializeEx(pythoncom.COINIT_MULTITHREADED)
             self.kompas = None
+            self.kompas_api7_module = None
             name = "Kompas.Application.7"
 
             try:
                 self.kompas = win32com.client.Dispatch(name)
                 self.kompas.Visible = True
+                self.kompas_api7_module = gencache.EnsureModule("{69AC2981-37C0-4379-84FD-5DD2F3C0A520}", 0, 1, 0)
             except Exception as e:
                 print(f"Не удалось подключиться к {name}: {e}")
 
@@ -28,16 +31,56 @@ class KompasService:
     def create_fragment(self, path):
         """Метод для создания фрагмента"""
         try:
-            new_doc = self.kompas.Documents.Add(2)  # 1 = фрагмент
+
+            doc = self.kompas.Documents.Add(2)
+
+            doc2d = self.kompas_api7_module.IKompasDocument2D(doc)
+
+            views = doc2d.ViewsAndLayersManager.Views[0]
+            m1 = views.MacroObjects.Add()
+
+            l1 = m1.LineSegments.Add()
+
+            l1.Style = 3
+            l1.X1 = -5
+            l1.Y1 = 0
+            l1.X2 = 5
+            l1.Y2 = 0
+
+            l1.Update()
+
+            l2 = m1.LineSegments.Add()
+
+            l2.Style = 3
+            l2.X1 = 0
+            l2.Y1 = -5
+            l2.X2 = 0
+            l2.Y2 = 5
+
+            l1.Update()
+
+            c1 = m1.Circles.Add()
+
+            c1.Style = 3
+            c1.Xc = 0
+            c1.Yc = 0
+            c1.Radius = 3
+
+            c1.Update()
+
+            m1.Name = "Ноль станка"
+            m1.Update()
 
             if not path.lower().endswith('.frw'):
                 path += '.frw'
 
-            new_doc.SaveAs(path)
-            print(f"Создан файл: {path}")
+            doc.SaveAs(path)
+            print(f"Создан файл с нулевой точкой: {path}")
 
         except Exception as e:
             print(f"Ошибка при создании фрагмента: {e}")
+            import traceback
+            traceback.print_exc()
 
     def open_fragment(self, path):
         """Метод для открытия фрагмента"""
